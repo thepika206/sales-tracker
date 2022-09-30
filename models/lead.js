92512a92d166e661c379264bb4b14776c8a27c30
pcheung <pika206@gmail.com>
2022-09-30 11:03:23 -0500

    implemented bootstrap navbar

---
+++ models/lead.js
@@ -0,0 +1,24 @@
+import mongoose from 'mongoose'
+
+const Schema = mongoose.Schema
+
+const leadSchema = new Schema({
+  name: String, 
+  owner: { type: Schema.Types.ObjectId, ref: 'Profile' },
+  //!basic setup for stubbing
+  //description: String,
+  // status: {
+  //   type:String,
+  //   enum: ['Closed-Won', 'Closed-Lost', 'Action-Needed', 'Waiting-for-Customer'],
+  //   }
+  //value: Number,
+  //comments: [commentSchema],
+}, {
+  timestamps: true,
+})
+
+const Lead = mongoose.model('Lead', leadSchema)
+
+export {
+  Lead
+}
