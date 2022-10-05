b1750aa3b9c0d689908385352a1ff278520b63a2
pcheung <pika206@gmail.com>
2022-10-05 07:43:31 -0500

    organize routes order and comments

--- routes/leads.js
+++ routes/leads.js
@@ -22,7 +22,7 @@ router.get('/my-leads-open', isLoggedIn, leadCtrl.indexMyLeadsOpen)
 // GET /leads/my-leads-closed
 router.get('/my-leads-closed', isLoggedIn, leadCtrl.indexMyLeadsClosed)
 
-// GET /leads/sales
+// GET /leads/my-sales
 router.get('/my-sales', isLoggedIn, leadCtrl.reportMySales)
 
 // GET /leads/new
@@ -34,9 +34,11 @@ router.get('/:leadId', isLoggedIn, leadCtrl.show)
 // GET /leads/:leadId/edit
 router.get('/:leadId/edit', isLoggedIn, leadCtrl.edit)
 
-
 // POST /leads
 router.post('/', isLoggedIn, leadCtrl.create)
+
+// POST /lead/:leadId/comments
+router.post('/:leadId/comments', isLoggedIn, leadCtrl.createComment)
 
 // PUT /leads/:leadId
 router.put('/:leadId', isLoggedIn, leadCtrl.update)
@@ -44,8 +46,6 @@ router.put('/:leadId', isLoggedIn, leadCtrl.update)
 // DELETE /leads/:leadId
 router.delete('/:leadId', isLoggedIn, leadCtrl.delete)
 
-// ADD COMMENTS POST /lead/:leadId/comments
-router.post('/:leadId/comments', isLoggedIn, leadCtrl.createComment)
 
 
 export {
