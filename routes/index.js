b80b7586a342d671ee6eef16e3c1bbcc1690a9ab
pcheung <pika206@gmail.com>
2022-10-05 07:44:20 -0500

    remove commented code in index router

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,6 @@ import { Router } from 'express'
 const router = Router()
 
 router.get('/', function (req, res) {
-  // res.redirect('/leads/my-leads')
   res.render('index', { title: 'Sales Tracker' })
 })
 
